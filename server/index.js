92501d7a2fea2b52652013dbbdc694ed5ea093fe
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-10 11:05:27 +0300

    feat:connect mongoDb using MONGO_URI in env file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 // const cors = require("cors");
+require("dotenv").config();
+const { connectDB } = require("./config/db-config");
 const mongoose = require("mongoose");
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
