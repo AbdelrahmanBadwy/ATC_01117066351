9bc43611f45696b3edfb16b190d9c6a62728ec52
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-10 18:12:30 +0300

    feat: integrates registration

---
+++ client/src/api-services/users-service.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const registerUser = async (data: never) => {
+  try {
+    console.log("Registering user with data:", data);
+    const response = await axios.post("/api/users/register", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw error;
+  }
+};
+export const loginUser = async (data: never) => {
+  try {
+    const response = await axios.post("/api/users/login", data);
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    throw error;
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 const { connectDB } = require("./config/db-config");
 const mongoose = require("mongoose");
@@ -7,11 +7,31 @@ const mongoose = require("mongoose");
 const userRouter = require("./routes/users-routes");
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 connectDB();
 app.use(express.json());
 app.use("/api/users", userRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
