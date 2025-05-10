f1014f7b19f5475d957fc33e61c2de349065ccd6
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-10 11:57:33 +0300

    feat: added user model in the backend

---
+++ server/models/user-model.ts
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    profilePicture: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
