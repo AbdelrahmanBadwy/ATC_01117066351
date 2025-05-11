af032dbcd6cc30d694fb6d3068160bd2e8802e16
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-11 12:18:59 +0300

    feat: handled layout responsiveness

--- client/src/layouts/sidebar/index.tsx
+++ client/src/layouts/sidebar/index.tsx
@@ -1,10 +1,35 @@
+import { Menu } from "lucide-react";
 import type { UserType } from "../../interfaces";
 import MenuItems from "./menu-items";
+import { useState } from "react";
+import { Drawer } from "antd";
 
 function Sidebar({ user }: { user: UserType }) {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <div className="w-60 h-full">
-      <MenuItems user={user} />
+    <div className="">
+      <div className="lg:flex hidden h-full lg:w-full">
+        <MenuItems user={user} />
+      </div>
+      <div className="bg-info p-5 lg:hidden flex">
+        <Menu
+          size={30}
+          className="text-white cursor-pointer"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        />
+      </div>
+      {showMobileMenu && (
+        <Drawer
+          title="Menu"
+          placement="left"
+          closable={true}
+          onClose={() => setShowMobileMenu(false)}
+          open={showMobileMenu}
+          width={300}
+        >
+          <MenuItems user={user} />
+        </Drawer>
+      )}
     </div>
   );
 }
--- client/src/layouts/sidebar/menu-items.tsx
+++ client/src/layouts/sidebar/menu-items.tsx
@@ -5,26 +5,35 @@ import {
   LogOut,
   BookCheck,
   UsersRound,
+  User,
 } from "lucide-react";
 import type { UserType } from "../../interfaces";
+import { useLocation, useNavigate } from "react-router-dom";
+import Cookie from "js-cookie";
 function MenuItems({ user }: { user: UserType }) {
   const iconSize = 20;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const currentPath = location.pathname;
   const userMenu = [
     {
       name: "Home",
       icon: <Home size={iconSize} />,
       path: "/",
       isActive: false,
+      icon: <User size={iconSize} />,
     },
     {
       name: "Bookings",
       icon: <List size={iconSize} />,
       path: "/bookings",
+      isActive: currentPath === "/bookings",
     },
     {
       name: "Reports",
       icon: <CandlestickChart size={iconSize} />,
       path: "/reports",
+      isActive: currentPath === "/reports",
     },
     {
       name: "Logout",
@@ -37,28 +46,31 @@ function MenuItems({ user }: { user: UserType }) {
       name: "Home",
       icon: <Home size={iconSize} />,
       path: "/",
-      isActive: false,
+      isActive: currentPath === "/",
     },
     {
       name: "Events",
       icon: <List size={iconSize} />,
       path: "/admin/events",
-      isActive: false,
+      isActive: currentPath.includes("/admin/events"),
     },
     {
       name: "Bookings",
       icon: <BookCheck size={iconSize} />,
       path: "/admin/bookings",
+      isActive: currentPath.includes("/admin/bookings"),
     },
     {
       name: "Users",
       icon: <UsersRound size={iconSize} />,
       path: "/admin/users",
+      isActive: currentPath.includes("/admin/users"),
     },
     {
       name: "Reports",
       icon: <CandlestickChart size={iconSize} />,
       path: "/admin/reports",
+      isActive: currentPath.includes("/admin/reports"),
     },
 
     {
@@ -69,8 +81,13 @@ function MenuItems({ user }: { user: UserType }) {
   ];
   const menu = user?.isAdmin ? adminMenu : userMenu;
 
+  const handleLogout = () => {
+    Cookie.remove("token");
+    navigate("/login");
+  };
+
   return (
-    <div className="bg-gray-200 h-full p-5">
+    <div className="md:lg:bg-gray-200 h-full p-5 w-full">
       <div className="flex flex-col gap-1 mt-5">
         <h1 className="text-2xl font-bold text-info">
           Badawy
@@ -80,9 +97,17 @@ function MenuItems({ user }: { user: UserType }) {
       </div>
       <div className="flex flex-col gap-10 mt-20">
         {menu.map((item: any) => (
-          <div className="flex gap-5 text-sm items-center" key={item.name}>
+          <div
+            className={`cursor-pointer px-5 py-3 rounded flex gap-5 text-sm items-center ${
+              item.isActive ? "bg-info text-white" : ""
+            }`}
+            key={item.name}
+            onClick={
+              item.name === "Logout" ? handleLogout : () => navigate(item.path)
+            }
+          >
             <span>{item.icon}</span>
-            <span className="text-sm text-gray-600">{item.name}</span>
+            <span>{item.name}</span>
           </div>
         ))}
       </div>
