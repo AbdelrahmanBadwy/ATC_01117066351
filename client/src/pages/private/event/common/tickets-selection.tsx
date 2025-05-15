726b53913e78ec0b57e34d206415245be326b64a
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-15 16:05:32 +0300

    feat:added show event details

---
+++ client/src/pages/private/event/common/tickets-selection.tsx
@@ -0,0 +1,8 @@
+import type { EventType } from "../../../../interfaces";
+
+
+function TicketsSelection({ eventData }: { eventData: EventType }) {
+  return <div>TicketsSelection</div>;
+}
+
+export default TicketsSelection;
