8988f2cb73437208178575053b0b3813ee3ae28a
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-15 11:20:56 +0300

    feat:added fetching events in homepage

--- client/src/pages/private/home/index.tsx
+++ client/src/pages/private/home/index.tsx
@@ -1,21 +1,41 @@
+import { useEffect, useState } from "react";
 import type { UserStoreType } from "../../../store/user-store";
 import useUserStore from "../../../store/user-store";
+import { message } from "antd";
+import { getEvents } from "../../../api-services/events-service";
+import EventCard from "./common/event-card";
+import type { EventType } from "../../../interfaces";
 // import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const [events, setEvents] = useState<EventType[]>([]);
+  const [loading, setLoading] = useState(true);
   const { currentUser } = useUserStore() as UserStoreType;
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await getEvents();
+      console.log("Response:", response);
+      setEvents(response.data);
+    } catch (error: any) {
+      message.error("Error fetching data:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <div className="mt-4">
-        {currentUser ? (
-          <div>
-            <h2 className="text-xl">Welcome, {currentUser?.name}!</h2>
-            <p>Email: {currentUser?.email}</p>
-          </div>
-        ) : (
-          <p>Loading user data...</p>
-        )}
+    <div>
+      <p>Welcome, {currentUser?.name}!</p>
+
+      <div className="flex flex-col gap-7">
+        {events.map((event) => (
+          <EventCard key={event._id} event={event} />
+        ))}
       </div>
     </div>
   );
