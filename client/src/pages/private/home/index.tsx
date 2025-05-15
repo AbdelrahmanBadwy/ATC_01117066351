eed6fa5da8491619248d7f6aae3b5d619950612f
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-15 11:37:59 +0300

    feat: added filters

--- client/src/pages/private/home/index.tsx
+++ client/src/pages/private/home/index.tsx
@@ -5,11 +5,16 @@ import { message } from "antd";
 import { getEvents } from "../../../api-services/events-service";
 import EventCard from "./common/event-card";
 import type { EventType } from "../../../interfaces";
+import Filters from "./common/filters";
 // import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
   const [events, setEvents] = useState<EventType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    searchText: "",
+    date: "",
+  });
   const { currentUser } = useUserStore() as UserStoreType;
 
   const getData = async () => {
@@ -32,7 +37,8 @@ function HomePage() {
     <div>
       <p>Welcome, {currentUser?.name}!</p>
 
-      <div className="flex flex-col gap-7">
+      <Filters filters={filters} setFilters={setFilters} onFilter={getData} />
+      <div className="flex flex-col gap-7 mt-7">
         {events.map((event) => (
           <EventCard key={event._id} event={event} />
         ))}
